04dacf3b4d1e60a196336028f74972b60b72be97
Dragoshel <ionescu.dragos23@gmail.com>
2023-11-08 20:32:32 +0100

    One HMM, 3 Hidden_States

--- hmm/hmm.py
+++ hmm/hmm.py
@@ -1,83 +1,42 @@
-from pomegranate.distributions import Normal
-from pomegranate.hmm import DenseHMM
+from pomegranate.distributions import Normal, Categorical
+from pomegranate.hmm import DenseHMM, SparseHMM
 
 from torchvision.transforms import ToTensor
 from torchvision.datasets import KMNIST, MNIST
-import torch
+from torch.utils.data import random_split
+from torch.utils.data import DataLoader
 
+import torch
 import numpy
 
-print("[INFO] loading the KMNIST test dataset...")
-train_data = KMNIST(root='data', train=True, download=True, transform=ToTensor())
-test_data = KMNIST(root='data', train=False, download=True, transform=ToTensor())
+N_HIDDEN_STATES = 3
+N_DIMENSIONS = 28 * 28
 
-model = DenseHMM([Normal(), Normal(), Normal(), Normal(), Normal(), Normal(), Normal(), Normal(), Normal(), Normal()], max_iter=10, verbose=True)
+TRAIN_SPLIT = 0.20
+VALID_SPLIT = 1 - TRAIN_SPLIT
 
-print("[INFO] initializing hmm...")
-model._initialize(torch.randn(60000, 784, 1))
-#model.fit(torch.randn(60000, 784, 1))
+print("[INFO] Loading the MNIST Dataset...")
+train_data = MNIST(root='data', train=True,
+                   download=True, transform=ToTensor())
+num_train_samples = int(len(train_data) * TRAIN_SPLIT)
+num_valid_samples = int(len(train_data) * VALID_SPLIT)
+(train_data, valid_data) = random_split(train_data,
+                                        [num_train_samples, num_valid_samples],
+                                        generator=torch.Generator().manual_seed(42))
 
-print("[INFO] processing images...")
-flattened_images = []
-for data, target in train_data:
-    image = data[0]
-    flattened_image = image.view(-1, 1)
-    flattened_images.append(flattened_image)
+print("[INFO] Initializing model...")
+train_data_loader = DataLoader(train_data, shuffle=True, batch_size=num_train_samples)
 
+# Create hmm with N_HIDDEN_STATES *empty* distributions
+# which map 1 to 1 to N_HIIDEN_STATES output nodes
+distributions = [Normal() for _ in range(N_HIDDEN_STATES)]
+model = DenseHMM(distributions, verbose=True)
 
-flattened_images = numpy.array(flattened_images)
-flattened_images = flattened_images[:1000]
+# Initialize the transition matrix with random values
+model.fit(torch.randn(1, N_DIMENSIONS, 1))
 
-print("[INFO] fitting HMM...")
-model.fit(flattened_images)
+for train_images, train_labels in train_data_loader:
+    train_images = train_images.reshape(-1, N_DIMENSIONS, 1)
 
-
-# from pomegranate.distributions import Categorical
-# from pomegranate.hmm import DenseHMM
-
-# import numpy
-
-# d1 = Categorical([[0.25, 0.25, 0.25, 0.25]])
-# d2 = Categorical([[0.10, 0.40, 0.40, 0.10]])
-
-# model = DenseHMM()
-# model.add_distributions([d1, d2])
-
-# model.add_edge(model.start, d1, 0.5)
-# model.add_edge(model.start, d2, 0.5)
-# model.add_edge(d1, d1, 0.9)
-# model.add_edge(d1, d2, 0.1)
-# model.add_edge(d2, d1, 0.1)
-# model.add_edge(d2, d2, 0.9)
-
-# X3 = torch.randn(1, 51, 1)
-# model.fit(X3)
-
-# sequence = 'CGACTACTGACTACTCGCCGACGCGACTGCCGTCTATACTGCGCATACGGC'
-# X = numpy.array([[[['A', 'C', 'G', 'T'].index(char)] for char in sequence]])
-# X.shape
-
-# #model.fit(X)
-# y_hat = model.predict(X)
-
-# print("sequence: {}".format(''.join(sequence)))
-# print("hmm pred: {}".format(''.join([str(y.item()) for y in y_hat[0]])))
-
-
-# from pomegranate.markov_chain import MarkovChain
-
-# model = MarkovChain(k=10)
-
-# print("[INFO] processing images...")
-# flattened_images = []
-# for data, target in train_data:
-#     image = data[0]
-#     flattened_image = image.view(-1, 1)
-#     flattened_images.append(flattened_image)
-
-# flattened_images = numpy.array(flattened_images)
-
-# model.fit(flattened_images)
-
-
-
+    print("[INFO] Fitting model...")
+    model.fit(train_images)
